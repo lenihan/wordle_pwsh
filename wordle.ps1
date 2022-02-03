afb265f8a08e692b00468cca932ac7ef3abb3700
David Lenihan <davidlenihan@outlook.com>
2022-02-03 10:43:06 -0800

    cleaned up logic, now under 30

--- wordle.ps1
+++ wordle.ps1
@@ -4,27 +4,26 @@ $actual = $words[(Get-Random -Maximum $words.Count)];
 $max_guess = if ($args[0] -eq "unlimit") {999999} else {6}
 for ($guess_count = 1; $guess_count -le $max_guess; $guess_count++) {
     $guess = (Read-Host -Prompt "Enter your guess ($guess_count / $max_guess)").ToLower()
-    if ($words -contains $guess) {
-        if ($actual -eq $guess) {
-            Write-Host "You guessed right!"
-            Write-Host $guess -ForegroundColor Black -BackgroundColor Green
-            return
-        } else {
-            $remaining = ""
-            for ($i = 0; $i -lt $actual.Length; $i++) {if ($actual[$i] -ne $guess[$i]) {$remaining += $actual[$i]}}
-            for ($i = 0; $i -lt $actual.Length; $i++) {
-                if ($actual[$i] -ne $guess[$i]) {
-                    if ($remaining.Contains($guess[$i])) {
-                        Write-Host $guess[$i] -ForegroundColor Black -BackgroundColor DarkYellow -NoNewLine
-                        $remaining = $remaining.Remove($remaining.IndexOf($guess[$i]), 1)
-                    } else {Write-Host $guess[$i] -ForegroundColor Black -BackgroundColor Gray -NoNewLine}
-                } else {Write-Host $guess[$i] -ForegroundColor Black -BackgroundColor Green -NoNewLine}
-            }
-            Write-Host
-        }
-    } else {
+    if ($words -notcontains $guess) {
         Write-Host "Please enter a valid word with 5 letters!"
         $guess_count--
+        continue
     }
+    if ($actual -eq $guess) {
+        Write-Host "You guessed right!"
+        Write-Host $guess -ForegroundColor Black -BackgroundColor Green
+        return
+    } 
+    $remaining = ""
+    for ($i = 0; $i -lt $actual.Length; $i++) {if ($actual[$i] -ne $guess[$i]) {$remaining += $actual[$i]}}
+    for ($i = 0; $i -lt $actual.Length; $i++) {
+        if ($actual[$i] -ne $guess[$i]) {
+            if ($remaining.Contains($guess[$i])) {
+                Write-Host $guess[$i] -ForegroundColor Black -BackgroundColor DarkYellow -NoNewLine
+                $remaining = $remaining.Remove($remaining.IndexOf($guess[$i]), 1)
+            } else {Write-Host $guess[$i] -ForegroundColor Black -BackgroundColor Gray -NoNewLine}
+        } else {Write-Host $guess[$i] -ForegroundColor Black -BackgroundColor Green -NoNewLine}
+    }
+    Write-Host
 }
 Write-Host "You lose! The word is: $actual"
